9c0d062726074459333aebab0d4c92fe26c5213e
alvaradojl <alvaradojl@outlook.com>
2018-07-15 21:28:56 +0200

    added docker login

--- deploy-test.sh
+++ deploy-test.sh
@@ -8,4 +8,6 @@ docker build -t alvaradojl/${PROJECT_NAME}:$TRAVIS_COMMIT .
 
 docker tag alvaradojl/${PROJECT_NAME}:$TRAVIS_COMMIT alvaradojl/${PROJECT_NAME}:latest
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker push alvaradojl/${PROJECT_NAME}
