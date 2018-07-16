de88338966f6805741c9823d6ade651491822133
alvaradojl <alvaradojl@outlook.com>
2018-07-16 12:31:21 +0200

    refactor config files

--- deploy-test.sh
+++ deploy-test.sh
@@ -2,20 +2,28 @@
 
 set -e
 
-pwd
-
+# build docker image and tag it to commit identifier
 docker build -t alvaradojl/${PROJECT_NAME}:$TRAVIS_COMMIT .
 
+# create a 'latest' tag for docker image
 docker tag alvaradojl/${PROJECT_NAME}:$TRAVIS_COMMIT alvaradojl/${PROJECT_NAME}:latest
 
+# login to docker cli
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# push docker image to registry
 docker push alvaradojl/${PROJECT_NAME}
 
+# login to azure cli
 az login --service-principal --username "$AZURE_USERNAME" --password "$AZURE_PASSWORD" --tenant "$AZURE_TENANT"
 
+# install kubectl from azure
 sudo az aks install-cli
 
-az aks get-credentials --resource-group RelayServices --name RelayServicesTest
+# embed credentials before using kubectl
+az aks get-credentials --resource-group "$AZURE_RESOURCEGROUPNAME" --name "$AZURE_KUBERNETESCLUSTERNAME"
 
-kubectl apply -f ./kubernetes.yaml
+# create kubernetes deployment
+kubectl apply -f ./k8s-deployment.yaml
+# create kubernetes service
+kubectl apply -f ./k8s-service.yaml
